744274514f8f99a76038ace2b36d79fd9f504bbd
yuye <1648655854@qq.com>
2022-01-25 16:24:50 +0800

    Initial commit

---
+++ src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * @author 数据组-雨叶
+ * @Description
+ * @created 2022-01-25 15:47
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
+
+}
