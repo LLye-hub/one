0e930c456c60b375a8ca794238a8df51005effd1
yuye <lilun.ye9193@hipac.cn>
2023-10-30 23:42:34 +0800

    算法题：【AC】阿里巴巴找黄金宝箱3

--- src/main/java/huaweiOdAC/IfExistNumber.java
+++ src/main/java/huaweiOdAC/IfExistNumber.java
@@ -1,4 +1,4 @@
-package huaweiOd;
+package huaweiOdAC;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -7,9 +7,10 @@ import java.util.Scanner;
 
 /**
  * @author 数仓开发-雨叶
- * @description 阿里巴巴找黄金宝箱5
- * 藏宝地有编号 0-n 的箱子，每个箱子贴有一个数字，念出一个咒语数字。
- * 是否存在两个不同的箱子，数字相同，且编号之差的绝对值小于等于咒语数字。
+ * @description 阿里巴巴找黄金宝箱3
+ * 一贫如洗的樵夫阿里巴巴在去砍柴的路上，无意中发现了强盗集团的藏宝地，藏宝地有编号从0~N的箱子，每个箱子上面贴有一个数字。
+ * 阿里巴巴念出一个咒语数字，查看宝箱是否存在两个不同箱子，这两个箱子上贴的数字相同，同时这两个箱子的编号之差的绝对值小于等于咒语数字。
+ * 如果存在这样的一对宝箱，请返回最先找到的那对宝箱左边箱子的编号，如果不存在则返回-1。
  * @case 6, 3, 1, 6
  * 3
  * return：0。存在，6
@@ -32,7 +33,7 @@ public class IfExistNumber {
 			if (!map.containsKey(num)) {
 				map.put(num, i);
 			} else if (i - map.get(num) <= diff) {
-				System.out.println(0);
+				System.out.println(map.get(num));
 				return;
 			}
 		}
